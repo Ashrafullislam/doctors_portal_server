1476c5abc667a0dee07ce7bc57d7c522a5fdbb4c
Md.Ashrafull Islam <ashrafull130@gmail.com>
2022-11-20 23:35:46 +0600

    make token and verify token after comming req from client side and give access after verify user with jwt token

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require ('cors')
 const port = process.env.PORT || 5000 ;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
+// json webtoken 
+const jwt = require('jsonwebtoken')
 
 // midleware
 app.use(cors())
@@ -14,17 +16,45 @@ app.get('/', (req, res) => {
     res.send("Doctors portal server running ")
 })
 
-// mongodb 
 
+
+
+// mongodb part 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rhjlmgh.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// verify user token 
+function verifyJWT(req,res,next) {
+    const authHeader = req.headers.authorization ;
+    if(!authHeader){
+         return  res.send(401).send('unauthorized access')
+
+    }
+    const token = authHeader.split(' ')[1]
+    // jwt verify  call for verify clien req token 
+    jwt.verify(token, process.env.ACCESS_TOKEN , function(err , decoded) {
+        // if an error occurd , then send the 403 status 
+        if(err) {
+            return res.status(403).send({message:'forbidden access'})
+        }
+        //  if not an error occurd doing work 
+        req.decoded = decoded ;
+
+        // next must be call for going to next step after verify 
+        next();
+        
+    })
+}
+
+
 
 const run = async() => {
     try {
      // all option collection 
      const appointmentOptionCollection = client.db('doctors_portal').collection('appointment_option') 
      const bookingsCollection = client.db('doctors_portal').collection('bookings')
+     const usersCollection = client.db('doctors_portal').collection('users')
      
      // get all appointment option from db 
     app.get('/appointment_option', async(req,res) =>  {
@@ -48,8 +78,24 @@ const run = async() => {
         res.send(options);
     })
     
+    // get booking data by specific user email  .... call the verifyJWT function 
+    app.get('/bookings',verifyJWT, async(req,res) => {
+        const email = req.query.email ;
+        // find decoded email to match with email 
+          const decodedEmail = req.decoded.email ;
+          console.log(decodedEmail,'decoded email ')
+        // // if decooded email and email are not match 
+        // if(decodedEmail !== email){
+        //     return res.status(403).send({message:'forbidden access '})
+        // }
+        // console.log('token check',req.headers.authorization)
+        console.log(email)
+        const query = {email:email};
+        const booking = await bookingsCollection.find(query).toArray()
+        res.send(booking)
+    })
 
-    // insert bookings data in db
+     // insert bookings data in db
     app.post('/bookings', async(req, res)=> {
         const booking = req.body ;
         // limit booking , if a user booking a service , he cannot able to again booking same treatment, same date 
@@ -57,7 +103,6 @@ const run = async() => {
         const query = {
             appointment_date: booking.appointment_date,
             appointmentName : booking.appointmentName ,
-       
         }
         const alreadyBooked = await bookingsCollection.find(query).toArray();
         // // only length means that length = 1 or equal
@@ -69,6 +114,32 @@ const run = async() => {
         const result = await bookingsCollection.insertOne(booking)
         res.send(result)
     })
+
+    //    *****  users save data and get data   ******
+    // json web token crete  and get token 
+    app.get('/jwt', async(req,res) => {
+        const email = req.query.email;
+        const query = {email:email};
+        const user = await usersCollection.findOne(query)
+        if(user){
+            const token = jwt.sign({email},process.env.ACCESS_TOKEN , {expiresIn:'1h'} )
+            return res.send({accessToken:token})
+        }
+        console.log(user)
+        res.status(403  ).send({accessToken:''})
+    })
+    
+
+    // post data in server to db 
+    app.post('/users', async(req,res) => {
+        const query = req.body ;
+        const userResult = await usersCollection.insertOne(query);
+        res.send(userResult)
+    })
+    
+    //  get data from data base 
+    
+
 
     }
     finally {
